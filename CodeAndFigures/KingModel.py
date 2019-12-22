ccae995a2c99f403c071c0c049f037686c28009e
Sandra Windows <sandra.b.glez@gmail.com>
2019-12-22 14:36:46 -0500

    Add plots and modified code to comply with pep257.

--- CodeAndFigures/KingModel.py
+++ CodeAndFigures/KingModel.py
@@ -3,9 +3,10 @@
 Created on Sat Dec  7 12:36:23 2019
 
 @author: Sandra Bustamante
+
 """
 
-import numpy as np
+# import numpy as np
 import matplotlib.pyplot as plt
 import NumericIntegrations as NI
 import SetupPlots as SP
@@ -13,109 +14,174 @@ import pandas as pd
 import time
 from scipy.special import erf
 from scipy.integrate import simps
-#import numpy.sqrt as sqrt
-#import numpy.exp as exp
-#import numpy.pi as pi
+from numpy import sqrt, exp, pi, abs, log10
+from numpy import zeros, arange, empty
+import matplotlib.colors as mcolors
+import matplotlib.cm as cm
 
-#%% Definitions
+# %% Definitions
 
-def dvdr(r,x,v,*args):
-    #For this case t=r, x=psi, v=dpsi/dr,dvdt=d^2psi/dr^2
-    d=(-4.*np.pi*G*rho(x,rho1,sigma)-(2.*v/r))
+
+def dvdr(r, x, v, *args):
+    """Define function to be integrated.
+
+    Parameters
+    ----------
+    r : TYPE
+        DESCRIPTION.
+    x : TYPE
+        DESCRIPTION.
+    v : TYPE
+        DESCRIPTION.
+    *args : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    d : TYPE
+        DESCRIPTION.
+
+    """
+    # For this case t=r, x=psi, v=dpsi/dr,dvdt=d^2psi/dr^2
+    d = (-4.*pi*G*rho(x, rho1, sigma)-(2.*v/r))
     return d
 
-def rho(x,rho1=1,sigma=1):
-    d=rho1*((np.exp(x/sigma**2.)*erf(np.sqrt(x)/sigma))
-            -(np.sqrt(4.*x/(np.pi*sigma**2.))*(1.+(2*x/(3.*sigma**2)))))
+
+def rho(x, rho1=1, sigma=1):
+    """Calculate the King density.
+
+    Parameters
+    ----------
+    x : TYPE
+        DESCRIPTION.
+    rho1 : TYPE, optional
+        DESCRIPTION. The default is 1.
+    sigma : TYPE, optional
+        DESCRIPTION. The default is 1.
+
+    Returns
+    -------
+    d : TYPE
+        DESCRIPTION.
+
+    """
+    d = rho1*((exp(x/sigma**2.)*erf(sqrt(x)/sigma))
+              - (sqrt(4.*x/(pi*sigma**2.))*(1.+(2*x/(3.*sigma**2)))))
     return d
 
-#%%
+
+# %%
 start = time.time()
 
-b=2.65
-h=0.001
-W0=np.arange(1.,8.75,.25)
-rho1,G,sigma=1.,1.,1.
+b = 2.7
+h = 0.0001
+W0 = arange(1., 8.75, .25)
+rho1, G, sigma = 1., 1., 1.
 
-s=(len(W0),int(b/h))
-#rtArray=np.zeros(len(W0))
-#xtArray=np.zeros(len(W0))
-#MArray=np.zeros(len(W0))
-#phi_rt=np.zeros(len(W0))
-#phi0=np.zeros(len(W0))
-rho0=np.zeros(len(W0))
-Etnorm=np.zeros(len(W0))
-rsq=np.zeros(len(W0))
-logr=np.zeros(len(W0))
-rArray=np.zeros(s)
-xArray=np.zeros(s)
-vArray=np.zeros(s)
+s = (len(W0), int(b/h))
 
+rho0 = zeros(len(W0))
+Etnorm = zeros(len(W0))
+rsq = zeros(len(W0))
+logr = zeros(len(W0))
 
-x0=W0*sigma**2 
-v0=0.
-rc=np.sqrt(9.*sigma**2/(4.*np.pi*G*rho(x0)))
+rxArray = empty(s)
+psiArray = empty(s)
+rhoArray = empty(s)
+
+x0 = W0*sigma**2
+v0 = 0.
+rc = sqrt(9.*sigma**2/(4.*pi*G*rho(x0)))
 
 for i in range(len(W0)):
-    IV=[x0[i],v0]
-    r,x,v=NI.RK4(dvdr,h,b,h,IV,dim=1)
-    #r,x,v=NI.leapfrog2D(dvdr,h,b,h,IV,dim=1)
-    rArray[i]=r
-    xArray[i]=x
-    vArray[i]=v
-    psi=x[x>0]
-    rx=r[x>0]
-    rt=rx[-1]
-    xt=psi[-1]
-    Mrt=4.*np.pi*simps(rho(psi)*rx**2,x=rx)
-    phi_rt=-G*Mrt/rt
-    phi0=np.abs(phi_rt-x0[i])
-    phi=phi_rt-psi
-    Et=np.pi*simps(phi*rho(psi)*rx**2,x=rx)
-    Etnorm[i]=Et/(G*Mrt**2/(rt))
-    logr[i]=np.log10(rt/rc[i])
-    rho0[i]=rho(x0[i])*4.*np.pi*rt**3/(3.*Mrt)
-    rsq[i]=(simps(rho(psi)*rx**4,x=rx)/simps(rho(psi)*rx**2,x=rx))/rt**2
+    IV = [x0[i], v0]
+    # r, x, v = NI.RK4(dvdr, h, b, h, IV, dim=1)
+    r, x, v = NI.leapfrog2D(dvdr, h, b, h, IV, dim=1)
+    psi = x[x > 0]
+    psiArray[i] = x
+    rx = r[x > 0]
+    rxArray[i] = r
+    rt = rx[-1]
+    xt = psi[-1]
+    Mrt = 4.*pi*simps(rho(psi)*rx**2, x=rx)
+    phi_rt = -G*Mrt/rt
+    phi0 = abs(phi_rt-x0[i])
+    phi = phi_rt-psi
+    Et = pi*simps(phi*rho(psi)*rx**2, x=rx)
+    Etnorm[i] = Et/(G*Mrt**2/(rt))
+    logr[i] = log10(rt/rc[i])
+    rho0[i] = rho(x0[i])*4.*pi*rt**3/(3.*Mrt)
+    rhoArray[i] = rho(x)
+    rsq[i] = (simps(rho(psi)*rx**4, x=rx)/simps(rho(psi)*rx**2, x=rx))/rt**2
 
 end = time.time()
-print('Time to run: %0.2f'%(end - start))
+print('Time to run: %0.2f' % (end - start))
+
+# %% Plot
+
+width, height = SP.setupPlot(singleColumn=False)
+fig = plt.figure()
+gs = fig.add_gridspec(2, 2, hspace=0, width_ratios=[10., .5])
+fig.set_size_inches((width, width))
+
+# setup the normalization and the colormap
+normalize = mcolors.Normalize(vmin=W0.min(), vmax=W0.max())
+colormap = cm.plasma
+
+ax1 = fig.add_subplot(gs[0, :1])
+ax2 = fig.add_subplot(gs[1, :1])
 for i in range(len(W0)):
-    print('W0:%1.2f, Etnorm:%1.2f, logr:%1.2f, rho0:%1.2e,rsq:%1.2e'%(W0[i],Etnorm[i],logr[i],rho0[i],rsq[i]))
+    ax1.semilogx(rxArray[i][rhoArray[i] > 0],
+                 log10(rhoArray[i][rhoArray[i] > 0]/rho0[i]),
+                 color=colormap(normalize(W0[i])))
+    ax2.semilogx(rxArray[i][rhoArray[i] > 0],
+                 psiArray[i][rhoArray[i] > 0],
+                 color=colormap(normalize(W0[i])))
 
-#print('x:',x)
+ax1.set_xlabel(r'$r$')
+ax1.set_ylabel(r'$\rho(r)/\rho(0)$')
+ax1.set_ylim((-6, 0))
+ax1.set_xticks([])
+ax1.grid()
 
-#%%
+ax2.set_ylabel(r'$\psi(r)$')
+ax2.grid()
 
-#plt.plot(phi0,logr)
-#plt.semilogx(r,xArray[-1])
-#plt.semilogx(r,xArray[0])
-#%% Save Data to csv file
+# setup the colorbar
+scalarmappaple = cm.ScalarMappable(norm=normalize, cmap=colormap)
+scalarmappaple.set_array(W0)
+axc = fig.add_subplot(gs[:2, 1])
+fig.colorbar(scalarmappaple, cax=axc, label='$W_0$')
 
-#Create a dictionary with column title in latex and variables 
-#can be a list or array.   
-    
-d = {'$W_0$': W0,
-     '$E_t/(GM^2/r_t)$': Etnorm,
-     '$\log_{10}(r_t/r_c)$': logr,
-     '$\rho(0)/(3M/(4\pi r_t^3))$': rho0,
-     '$<r^2>/r_t^2$': rsq }
+fig.tight_layout()
+fig.savefig('KingModelPlots.pdf')
 
-#Create the table in tabular form
+# %% Save Data to latex table.
+
+# Create a dictionary with column title in latex and variables
+# can be a list or array.
+
+d = {r'$W_0$': W0,
+     r'$E_t/(GM^2/r_t)$': Etnorm,
+     r'$\log_{10}(r_t/r_c)$': logr,
+     r'$\rho(0)/(3M/(4\pi r_t^3))$': rho0,
+     r'$<r^2>/r_t^2$': rsq}
+
+# Create the table in tabular form
 df = pd.DataFrame(d)
 
-#Create the file.tex 
-with open('KingModelTable.tex','w') as tf:
+print(df)
+
+# Create the file.tex
+with open('KingModelTable.tex', 'w') as tf:
     tf.write(df.to_latex(float_format='%2.2e',
                          index=False,
                          escape=False))
-    
-#On latex, just do
-#\begin{table}[]
+
+# In latex, just do:
+# \begin{table}[]
 #    \centering
-#    \include{KingModel.tex}
+#    \input{KingModel.tex}
 #    \caption{Caption}
 #    \label{tab:my_label}
-#\end{table}
-    
-
-
+# \end{table}
--- CodeAndFigures/NumericIntegrations.py
+++ CodeAndFigures/NumericIntegrations.py
@@ -3,72 +3,102 @@
 """
 Created on Mon Oct 28 17:08:16 2019
 
-@author: sbustamanteg
+@author: Sandra Bustamante
 """
 
 import numpy as np
 
-def leapfrog2D(dvdt,a,b,h,IV,dim=2):
-  #h = (b-a)/float(N)
-  t=np.arange(a,b+h,h)
-  x=np.zeros((len(t),dim))
-  v=np.zeros((len(t),dim))
-  x[0], v[0] = IV
-  for n in np.arange(1,len(t)):
-    k1=x[n-1]+0.5*h*v[n-1]
-    v[n]=v[n-1]+h*dvdt(t,k1,v)
-    x[n]=k1+0.5*h*v[n]
-  return t, x, v
 
-#def RK4(dvdt, a, b, h, IV,dim=2):
-#    t = np.arange(a,b+h,h)  # create time
-#    #N=int((b-a)/h)
-#    x = np.zeros((len(t),dim))       # initialize x
-#    v = np.zeros((len(t),dim))       # initialize x
-#    x[0], v[0] = IV       # set initial values
-#    # apply Fourth Order Runge-Kutta Method
-#    for i in np.arange(1,len(t)):
-#        k1= h*dvdt(t[i-1], x[i-1], v[i-1])
-#        j1= h*v[i-1]
-#        k2= h*dvdt(t[i-1]+h/2.0, x[i-1]+j1/2.0,
-#                   v[i-1]+k1/2.0)
-#        j2= h*(v[i-1]+k1/2.0)
-#        k3= h*dvdt(t[i-1]+h/2.0, x[i-1]+j2/2.0,
-#                   v[i-1]+k2/2.0)
-#        j3= h*(v[i-1]+k2/2.0)
-#        k4= h*dvdt( t[i], x[i-1] + j3, v[i-1] + k3)
-#        j4= h*(v[i-1] + k3)
-#        v[i] =v[i-1] + (k1 + 2.0*k2 + 2.0*k3 + k4)/6.0
-#        x[i] =x[i-1] + (j1 + 2.0*j2 + 2.0*j3 + j4)/6.0
-#    return t, x, v
+def leapfrog2D(dvdt, a, b, h, IV, dim=2):
+    """Integrate using 2nd order Leapfrog.
 
-def RK4(dvdt, a, b, h, IV,dim=2):
-    t = np.arange(a,b+h,h)  # create time
-    #N=int((b-a)/h)
-    x = np.zeros((len(t),dim))       # initialize x
-    v = np.zeros((len(t),dim))       # initialize x
-    x[0], v[0] = IV       # set initial values
+    Parameters
+    ----------
+    dvdt : TYPE
+        DESCRIPTION.
+    a : TYPE
+        DESCRIPTION.
+    b : TYPE
+        DESCRIPTION.
+    h : TYPE
+        DESCRIPTION.
+    IV : TYPE
+        DESCRIPTION.
+    dim : TYPE, optional
+        DESCRIPTION. The default is 2.
+
+    Returns
+    -------
+    t : TYPE
+        DESCRIPTION.
+    x : TYPE
+        DESCRIPTION.
+    v : TYPE
+        DESCRIPTION.
+
+    """
+    # h = (b-a)/float(N)
+    t = np.arange(a, b+h, h)
+    x = np.zeros((len(t), dim))
+    v = np.zeros((len(t), dim))
+    x[0], v[0] = IV
+    for n in np.arange(1, len(t)):
+        k1 = x[n-1]+0.5*h*v[n-1]
+        v[n] = v[n-1]+h*dvdt(t, k1, v)
+        x[n] = k1+0.5*h*v[n]
+    return t, x, v
+
+
+def RK4(dvdt, a, b, h, IV, dim=2):
+    """Integrate 1st Order Diferrential Equations using RK4.
+
+    Parameters
+    ----------
+    dvdt : TYPE
+        DESCRIPTION.
+    a : TYPE
+        DESCRIPTION.
+    b : TYPE
+        DESCRIPTION.
+    h : TYPE
+        DESCRIPTION.
+    IV : TYPE
+        DESCRIPTION.
+    dim : TYPE, optional
+        DESCRIPTION. The default is 2.
+
+    Returns
+    -------
+    t : TYPE
+        DESCRIPTION.
+    x : TYPE
+        DESCRIPTION.
+    v : TYPE
+        DESCRIPTION.
+
+    """
+    t = np.arange(a, b+h, h)          # create time
+    x = np.zeros((len(t), dim))      # initialize x
+    v = np.zeros((len(t), dim))      # initialize x
+    x[0], v[0] = IV                 # set initial values
     # apply Fourth Order Runge-Kutta Method
-    for i in np.arange(1,len(t)):
-        if x[i-1]>=0:
-            k1= h*dvdt(t[i-1], x[i-1], v[i-1])
-            j1= h*v[i-1]
-            k2= h*dvdt(t[i-1]+h/2.0, x[i-1]+j1/2.0,
-                       v[i-1]+k1/2.0)
-            j2= h*(v[i-1]+k1/2.0)
-            k3= h*dvdt(t[i-1]+h/2.0, x[i-1]+j2/2.0,
-                       v[i-1]+k2/2.0)
-            j3= h*(v[i-1]+k2/2.0)
-            k4= h*dvdt( t[i], x[i-1] + j3, v[i-1] + k3)
-            j4= h*(v[i-1] + k3)
-            v[i] =v[i-1] + (k1 + 2.0*k2 + 2.0*k3 + k4)/6.0
-            x[i] =x[i-1] + (j1 + 2.0*j2 + 2.0*j3 + j4)/6.0
-    #        if i==1:
-    #            print('ks',k1,k2,k3,k4)
-    #            print('js',j1,j2,j3,j4)
+    for i in np.arange(1, len(t)):
+        if x[i-1] >= 0:
+            k1 = h*dvdt(t[i-1], x[i-1], v[i-1])
+            j1 = h*v[i-1]
+            k2 = h*dvdt(t[i-1]+h/2.0, x[i-1]+j1/2.0,
+                        v[i-1] + k1/2.0)
+            j2 = h*(v[i-1]+k1/2.0)
+            k3 = h*dvdt(t[i-1]+h/2.0, x[i-1]+j2/2.0,
+                        v[i-1] + k2/2.0)
+            j3 = h*(v[i-1]+k2/2.0)
+            k4 = h*dvdt(t[i], x[i-1] + j3, v[i-1] + k3)
+            j4 = h*(v[i-1] + k3)
+            v[i] = v[i-1] + (k1 + 2.0*k2 + 2.0*k3 + k4)/6.0
+            x[i] = x[i-1] + (j1 + 2.0*j2 + 2.0*j3 + j4)/6.0
         else:
             break
-    if dim==1:
-        x=x.reshape(len(t))
-        v=v.reshape(len(t))
+    if dim == 1:
+        x = x.reshape(len(t))
+        v = v.reshape(len(t))
     return t, x, v
